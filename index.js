b478c39268110f44d926b50eacd6c9b166ad08d1
Laura <laurao148@gmail.com>
2018-08-16 02:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function lowerCaseDrivers(drivers) {
-  const newArrayDrivers = drivers.map(function(driver)) {
+  const newArrayDrivers = drivers.map(function(driver) {
     return driver.toLowerCase();
   })
   return newArrayDrivers;
