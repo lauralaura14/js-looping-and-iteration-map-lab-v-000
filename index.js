d9395899e969c5b77eb9dd0d7893dd331e15c156
Laura <laurao148@gmail.com>
2018-08-16 02:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  const newSentence = drivers.map(function(driver) {
+  return drivers.map(function(driver) {
     return `${driver.name} is from ${driver.hometown}`;
   })
-  const newSentence;
 }
