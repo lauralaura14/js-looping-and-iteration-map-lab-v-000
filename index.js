5d51f789583237db0333b43c286a09441c44f8b9
Laura <laurao148@gmail.com>
2018-08-16 02:23:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function lowerCaseDrivers(drivers) {
   })
   return newArrayDrivers;
 }
+
+function nameToAttributes(drivers) {
+  const newList = drivers.map(function(driver) {
+    return Object.assign({}, firstName: name.split(" ")[0], lastName: name.split(" ").[1]);
+  }
+  return newList;
+}
